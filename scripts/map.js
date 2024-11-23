f047c922890a69c8d086091c26e039287eba16ee
Vadym Kostiuk <112824921+kastillio@users.noreply.github.com>
2024-11-22 23:16:34 -0500

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -9,7 +9,7 @@ const textData = {
         'bakery': { name: "Пекарня", description: "Як виготовляли хліб у давні часи?" },
         'spices': { name: "Магазин спецій", description: "Що могли розповісти спеції про людей?" },
         'wise-men': { name: "Мудреці", description: "Що мудреці принесли до Вифлеєму і чому це було важливо?" },
-        'stable': { name: "Стайня", description: "Дізнайтеся, чому саме стайня стала колискою для Ісуса." },
+        'spinning Wheel': { name: "Прядильне колесо", description: "Дізнайтеся, як створювали одяг у стародавні часи, та виберіть костюм для фотозони!" },
         'photo-zone': { name: "Фотозона", description: "Зробіть фото, яке збереже цю мить!" }
     },
     en: {
@@ -21,7 +21,7 @@ const textData = {
         'bakery': { name: "Bakery", description: "How was bread made in ancient times?" },
         'spices': { name: "Spice Market", description: "What could spices reveal about the people who used them?" },
         'wise-men': { name: "Wise Men", description: "What gifts did the Wise Men bring, and why were they significant?" },
-        'spinningWheel': { name: " spinningWheel", description: "Discover why the stable became the cradle for Jesus." },
+        'spinningWheel': { name: " spinningWheel", description: "Children will learn how the spinning wheel was used in ancient times to make clothing." },
         'photo-zone': { name: "Photo Zone", description: "Take a picture to remember this special moment forever!" }
     }
 };
